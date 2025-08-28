6ae8a46c43133c698401f32d88b1a260f2410b9e
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2025-08-28 10:54:18 -0300

    mas

---
+++ Dominio/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace Dominio
+{
+    public class Auto
+    {
+        private string _marca;
+        private string _modelo;
+        private bool _exoneraImpuestos;
+        private string _matricula;
+
+        public Auto(string marca, string modelo, bool exoneraImpuestos, string matricula)
+        {
+            _marca = marca;
+            _modelo = modelo;
+            _exoneraImpuestos = exoneraImpuestos;
+            _matricula = matricula;
+               
+        }
+        public void ValidarMatricula()
+        {
+            // Supongamos que la matrícula debe tener 7 caracteres.
+            if (_matricula.Length != 7)
+            {
+                throw new ArgumentException("La matrícula debe tener 7 caracteres.");
+            }
+
+            // Si no hay errores, el método simplemente termina.
+            // No devuelve nada.
+        }
+
+        public decimal CalcularPatente()
+        {
+
+        }
+    }
+}
